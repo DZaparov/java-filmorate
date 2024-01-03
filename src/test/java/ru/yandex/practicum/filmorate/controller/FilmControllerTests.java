6c5ab0e14b638c5746a2377138f9050214fca03a
DZaparov <dimaxiz1989@yandex.ru>
2024-01-03 20:06:48 +0300

    feat: REST контроллеры, валидация, логирование, тесты

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
@@ -0,0 +1,104 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FilmControllerTests {
+    private FilmController filmController;
+
+    @BeforeEach
+    void BeforeEach() {
+        filmController = new FilmController();
+    }
+
+    @Test
+    void validateFalseIfFilmWithEmptyNameTest() {
+        try {
+            Film film = new Film(null, "", "Описание", LocalDate.of(2000, 12, 12), 26);
+            assertFalse(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateTrueIfFilmWithNoEmptyNameTest() {
+        try {
+            Film film = new Film(null, "Название", "Описание", LocalDate.of(2000, 12, 12), 26);
+            assertTrue(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateFalseIfFilmWithDescriptionLengthMoreThan200Test() {
+        try {
+            Film film = new Film(null, "Название",
+                    "ОписаниеОписаниеОписаниеОписаниеОписаниеОписаниеОписание" +
+                            "ОписаниеОписаниеОписаниеОписаниеОписаниеОписаниеОписание" +
+                            "ОписаниеОписаниеОписаниеОписаниеОписаниеОписаниеОписание" +
+                            "ОписаниеОписаниеОписаниеОписание1", LocalDate.of(2000, 12, 12), 26);
+            assertFalse(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateTrueIfFilmWithDescriptionLength200Test() {
+        try {
+            Film film = new Film(null, "Название",
+                    "ОписаниеОписаниеОписаниеОписаниеОписаниеОписаниеОписание" +
+                            "ОписаниеОписаниеОписаниеОписаниеОписаниеОписаниеОписание" +
+                            "ОписаниеОписаниеОписаниеОписаниеОписаниеОписаниеОписание" +
+                            "ОписаниеОписаниеОписаниеОписание", LocalDate.of(2000, 12, 12), 26);
+            assertTrue(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateFalseIfFilmWithDateBefore28dec1895Test() {
+        try {
+            Film film = new Film(null, "Название", "Описание", LocalDate.of(1895, 12, 27), 26);
+            assertFalse(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateTrueIfFilmWithDate28dec1895Test() {
+        try {
+            Film film = new Film(null, "Название", "Описание", LocalDate.of(1895, 12, 28), 26);
+            assertTrue(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateFalseIfFilmDurationIsZeroOrMinusTest() {
+        try {
+            Film film = new Film(null, "Название", "Описание", LocalDate.of(1895, 12, 28), 0);
+            assertFalse(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+        try {
+            Film film2 = new Film(null, "Название", "Описание", LocalDate.of(1895, 12, 28), -1);
+            assertFalse(filmController.validateFilm(film2));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateTrueIfFilmDurationIsPlusTest() {
+        try {
+            Film film = new Film(null, "Название", "Описание", LocalDate.of(1895, 12, 28), 26);
+            assertTrue(filmController.validateFilm(film));
+        } catch (Exception ignored) {
+        }
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTests.java
@@ -0,0 +1,102 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserControllerTests {
+    private UserController userController;
+
+    @BeforeEach
+    void BeforeEach() {
+        userController = new UserController();
+    }
+
+    @Test
+    void validateFalseIfUserWithEmptyEmailTest() {
+        try {
+            User user = new User(null, "", "login123", "Имя", LocalDate.of(2000, 12, 12));
+            assertFalse(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateFalseIfUserWithoutAtInEmailTest() {
+        try {
+            User user = new User(null, "qwerty.ru", "login123", "Имя", LocalDate.of(2000, 12, 12));
+            assertFalse(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateTrueIfUserWithGoodEmailTest() {
+        try {
+            User user = new User(null, "user@qwerty.ru", "login123", "Имя", LocalDate.of(2000, 12, 12));
+            assertTrue(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateFalseIfUserWithEmptyLoginTest() {
+        try {
+            User user = new User(null, "user@qwerty.ru", "", "Имя", LocalDate.of(2000, 12, 12));
+            assertFalse(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateFalseIfUserWithSpaceInLoginTest() {
+        try {
+            User user = new User(null, "user@qwerty.ru", " login123", "Имя", LocalDate.of(2000, 12, 12));
+            assertFalse(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateTrueIfUserWithGoodLoginTest() {
+        try {
+            User user = new User(null, "user@qwerty.ru", "login123", "Имя", LocalDate.of(2000, 12, 12));
+            assertTrue(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateIfUserNameIsEmptyTest() {
+        try {
+            User user = new User(null, "user@qwerty.ru", "login123", "", LocalDate.of(2000, 12, 12));
+            assertTrue(userController.validateUser(user));
+            assertEquals(user.getLogin(), user.getName(), "При пустом имени должен использоваться логин");
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateFalseIfUserBirthdayInFutureTest() {
+        try {
+            User user = new User(null, "user@qwerty.ru", "login123", "Имя", LocalDate.now().plusDays(1));
+            assertFalse(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+
+    @Test
+    void validateTrueIfUserBirthdayInPastTest() {
+        try {
+            User user = new User(null, "user@qwerty.ru", "login123", "Имя", LocalDate.now().minusDays(1));
+            assertTrue(userController.validateUser(user));
+        } catch (Exception ignored) {
+        }
+    }
+}
